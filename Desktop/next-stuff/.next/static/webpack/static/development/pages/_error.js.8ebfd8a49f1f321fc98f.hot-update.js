46518d374c78a82b207ced4195a812f68b93b3f6
Dan McKinney <3dhdaniel@gmail.com>
2019-06-11 15:34:45 -0400

    next

---
+++ Desktop/next-stuff/.next/static/webpack/static/development/pages/_error.js.8ebfd8a49f1f321fc98f.hot-update.js
@@ -0,0 +1,170 @@
+webpackHotUpdate("static/development/pages/_error.js",{
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/array/from.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/is-iterable.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/arrayWithoutHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/assertThisInitialized.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/iterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/nonIterableSpread.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/setPrototypeOf.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/toConsumableArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/typeof.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/array/from.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/is-iterable.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_create-property.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/core.is-iterable.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.array.from.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/amp.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/amphtml-context.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/head-manager-context.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/head.js":
+false,
+
+/***/ "./node_modules/next-server/dist/lib/side-effect.js":
+false,
+
+/***/ "./node_modules/next-server/head.js":
+false,
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fdanielmckinney%2FDesktop%2Fnext-stuff%2Fpages%2F_error.js!./":
+/*!***************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fdanielmckinney%2FDesktop%2Fnext-stuff%2Fpages%2F_error.js ***!
+  \***************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/_error", function() {
+      var page = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/_error.js */ "./pages/_error.js", function() {
+          if(!next.router.components["/_error"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+          next.router.update("/_error", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=next%2Fdist%2Fpages%2F_error!./":
+false,
+
+/***/ "./node_modules/next/dist/pages/_error.js":
+false,
+
+/***/ "./pages/_error.js":
+/*!*************************!*\
+  !*** ./pages/_error.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/danielmckinney/Desktop/next-stuff/pages/_error.js";
+
+
+
+var errorPage = function errorPage() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, "Oops something went wrong"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Try ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Going back"))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (errorPage);
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fdanielmckinney%2FDesktop%2Fnext-stuff%2Fpages%2F_error.js ***!
+  \*******************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fdanielmckinney%2FDesktop%2Fnext-stuff%2Fpages%2F_error.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fdanielmckinney%2FDesktop%2Fnext-stuff%2Fpages%2F_error.js!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_error.js.8ebfd8a49f1f321fc98f.hot-update.js.map
---
+++ Desktop/next-stuff/.next/static/webpack/static/development/pages/auth.js.b6650e6c0577eed01feb.hot-update.js
@@ -0,0 +1,53 @@
+webpackHotUpdate("static/development/pages/auth.js",{
+
+/***/ "./components/User.js":
+/*!****************************!*\
+  !*** ./components/User.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/danielmckinney/Desktop/next-stuff/components/User.js";
+
+
+
+var user = function user(props) {
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "jsx-4194195407",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("h1", {
+    className: "jsx-4194195407",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, props.name), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("p", {
+    className: "jsx-4194195407",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Age: ", props.age), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    id: "4194195407",
+    __self: this
+  }, "div.jsx-4194195407{border:1px solid #eee;box-shadow:0 2px 3px #ccc;padding:2px;text-align:center;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9Vc2Vycy9kYW5pZWxtY2tpbm5leS9EZXNrdG9wL25leHQtc3R1ZmYvY29tcG9uZW50cy9Vc2VyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQVFhLEFBRzJDLHNCQUNJLDBCQUNkLFlBRWpCLGtCQUFDIiwiZmlsZSI6Ii9Vc2Vycy9kYW5pZWxtY2tpbm5leS9EZXNrdG9wL25leHQtc3R1ZmYvY29tcG9uZW50cy9Vc2VyLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuXG5jb25zdCB1c2VyID0gKHByb3BzKSA9PiAoXG4gICAgPGRpdj5cbiAgICAgICAgPGgxPntwcm9wcy5uYW1lfTwvaDE+XG4gICAgICAgIDxwPkFnZToge3Byb3BzLmFnZX08L3A+XG4gICAgICAgIDxzdHlsZSBqc3g+XG4gICAgICAgICAgICB7YFxuICAgICAgICAgICAgICAgIGRpdiB7XG4gICAgICAgICAgICAgICAgICAgIGJvcmRlcjogMXB4IHNvbGlkICNlZWU7XG4gICAgICAgICAgICAgICAgICAgIGJveC1zaGFkb3c6IDAgMnB4IDNweCAjY2NjO1xuICAgICAgICAgICAgICAgICAgICBwYWRkaW5nOiAycHg7XG4gICAgICAgICAgICAgICAgICAgIHRleHQtYWxpZ246IGNlbnRlclxuICAgICAgICAgICAgICAgIH1cbiAgICAgICAgICAgIGB9XG4gICAgICAgIDwvc3R5bGU+XG4gICAgPC9kaXY+XG4pO1xuXG5cbmV4cG9ydCBkZWZhdWx0IHVzZXIiXX0= */\n/*@ sourceURL=/Users/danielmckinney/Desktop/next-stuff/components/User.js */"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (user);
+
+/***/ })
+
+})
+//# sourceMappingURL=auth.js.b6650e6c0577eed01feb.hot-update.js.map
