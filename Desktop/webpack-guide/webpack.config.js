4c38fbdf0382126ebbf17f61c55eddff206e6fd1
Dan McKinney <3dhdaniel@gmail.com>
2019-06-10 22:14:14 -0400

    webpacl

--- Desktop/webpack-guide/webpack.config.js
+++ Desktop/webpack-guide/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -17,7 +19,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                loader:  'babel-loader',
+                loader: 'babel-loader',
                 exclude: /node_modules/
             },
             {
@@ -25,29 +27,28 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     { loader: 'style-loader' },
-                    {   
+                    { 
                         loader: 'css-loader',
                         options: {
                             importLoaders: 1,
                             modules: true,
-                            localIdentName: '[name]__[local][hash:base64:5]'
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
                         }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [
-                                autoprefixer({
-                                    broswers: [
+                     },
+                     { 
+                         loader: 'postcss-loader',
+                         options: {
+                             ident: 'postcss',
+                             plugins: () => [
+                                 autoprefixer({
+                                     browsers: [
                                         "> 1%",
                                         "last 2 versions"
-                                    ]
-                                })
-                            ]
-                        },
-                        
-                    }
+                                     ]
+                                 })
+                             ]
+                         }
+                      }
                 ]
             },
             {
@@ -55,5 +56,13 @@ module.exports = {
                 loader: 'url-loader?limit=8000&name=images/[name].[ext]'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
