c8d5750b31e866e959cb48073219f65f55468b46
Dan McKinney <3dhdaniel@gmail.com>
2019-06-12 22:33:31 -0400

    momo

--- Desktop/auth--02-auth-store/src/store/actions/auth.js
+++ Desktop/auth--02-auth-store/src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 import * as actionTypes from './actionTypes';
 
@@ -49,32 +49,12 @@ export const checkAuthTimeout = (expirationTime) => {
 
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA6tq7PUPy9NO-psaELogwmghorvtFkGNk';
-        if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyA6tq7PUPy9NO-psaELogwmghorvtFkGNkY';
-        }
-        axios.post(url, authData)
-            .then(response => {
-                // console.log(response);
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
-                localStorage.setItem('token', response.data.idToken)
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId)
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn))
-            })
-            .catch(err => {
-                // console.log(err);
-                dispatch(authFail(err.response.data.error));
-            });
-    };
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
+    }
 };
 
 export const setAuthRedirectPath = (path) => {
