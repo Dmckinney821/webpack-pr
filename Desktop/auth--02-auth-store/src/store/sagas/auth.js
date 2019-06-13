966a96844acb78a15cfa5c8b0dea4d8610834535
Dan McKinney <3dhdaniel@gmail.com>
2019-06-12 22:12:36 -0400

    wed

---
+++ Desktop/auth--02-auth-store/src/store/sagas/auth.js
@@ -0,0 +1,18 @@
+import { put } from 'redux-saga/effects'
+import * as actions from '../actions/index'
+import { delay } from 'redux-saga/effects'
+
+
+export function* logoutSaga(action) {
+    yield localStorage.removeItem('token')
+    yield localStorage.removeItem('expirationDate')
+    yield localStorage.removeItem('userId');
+    yield put(actions.logoutSucceed());
+}
+
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(action.expirationTime)
+    yield put(actions.logout());
+    
+}
